b144d32bc84da9b9b14fb6730f2524bb3d55f167
saulo <sauloandrade0809@gmail.com>
2019-08-08 22:12:58 -0300

    pp_2_08_08_2019

---
+++ ex/component.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export default function() {
+    return <h1>Primeiro Componente! Vai funcionar?</h1>
+}
---
+++ ex/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Exemplo from './component.jsx'
+
+ReactDOM.render(<Exemplo/>,
+     document.getElementById('app'))
